9d320e9bd0510bb2c9895e41df3c5ae76b939c50
iahmedgamal <ahmad.g.mustafa@gmail.com>
2024-08-15 15:00:24 +0300

    better navigation with icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ function Header() {
   return (
     <header className="bg-slate-100 text-teal-600 p-4 md:p-6 fixed top-0 inset-x-0 z-10 shadow-sm">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl md:text-3xl font-bold">User Profile Dashboard</h1>
+      <h1 className="text-xl md:text-2xl font-bold">User Profile Dashboard</h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { User } from "../interfaces/user.interface";
-
+import { HiMenu, HiX } from "react-icons/hi";
 
 interface SidebarProps {
   onUserSelect: (user: User) => void;
@@ -13,7 +13,6 @@ const SideBar = ({ onUserSelect }: SidebarProps) => {
   const [error, setError] = useState<string | null>(null);
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
 
-  
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -42,60 +41,64 @@ const SideBar = ({ onUserSelect }: SidebarProps) => {
     setIsSidebarOpen((prev) => !prev);
   };
 
-  
   if (loading) {
     return (
       <aside
-      className={`bg-slate-100 w-full md:w-64 p-4 text-teal-500 fixed top-16 bottom-0 left-0 shadow-lg md:relative transition-transform duration-300 ${
-        isSidebarOpen ? "transform translate-x-0" : "transform -translate-x-full"
-      }`}
-    >
-      Loading users...
-    </aside>
+        className={`bg-slate-100 w-full md:w-64 p-4 text-teal-500 fixed top-16 bottom-0 left-0 shadow-lg md:relative transition-transform duration-300 ${
+          isSidebarOpen
+            ? "transform translate-x-0"
+            : "transform -translate-x-full"
+        }`}
+      >
+        Loading users...
+      </aside>
     );
   }
 
   if (error) {
     return (
       <aside
-      className={`bg-slate-100 w-full md:w-64 p-4 text-teal-500 fixed top-16 bottom-0 left-0 shadow-lg md:relative transition-transform duration-300 ${
-        isSidebarOpen ? "transform translate-x-0" : "transform -translate-x-full"
-      }`}
-    >
-      Error: {error}
-    </aside>
+        className={`bg-slate-100 w-full md:w-64 p-4 text-teal-500 fixed top-16 bottom-0 left-0 shadow-lg md:relative transition-transform duration-300 ${
+          isSidebarOpen
+            ? "transform translate-x-0"
+            : "transform -translate-x-full"
+        }`}
+      >
+        Error: {error}
+      </aside>
     );
   }
 
   return (
     <>
-     <button
+   <button
         onClick={toggleSidebar}
-        className="fixed top-4 left-4 md:hidden p-2 bg-teal-600 text-white rounded-md shadow-md z-50"
+        className="fixed top-4 left-4 md:hidden p-2 bg-teal-600 text-white rounded-full shadow-md z-10 flex items-center justify-center w-10 h-10"
       >
-        {isSidebarOpen ? "Close" : "Open"}
+        {isSidebarOpen ? <HiX className="w-6 h-6" /> : <HiMenu className="w-6 h-6" />}
       </button>
 
-      <aside     className={`bg-slate-100 w-full md:w-64 p-4 text-teal-500 fixed top-16 bottom-0 left-0 shadow-lg md:relative transition-transform duration-300 ${
+      <aside
+        className={`bg-slate-100 md:w-64 p-6 text-teal-500 fixed top-16 bottom-0 left-0 shadow-lg md:relative transition-transform duration-300 ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-        } md:translate-x-0`}>
-      <h2 className="text-lg font-bold mb-4">Users</h2>
-      <ul>
-        {users.map((user) => (
-          <li
-            key={user.id}
-            className={`p-2 cursor-pointer ${
-              selectedUserId === user.id ? "bg-teal-600 text-white" : ""
-            } hover:bg-teal-100 transition-colors`}
-            onClick={() => handleUserClick(user)}
-          >
-            {user.name}
-          </li>
-        ))}
-      </ul>
-    </aside>
+        } md:translate-x-0`}
+      >
+        <h2 className="text-lg font-bold mb-4">Users</h2>
+        <ul>
+          {users.map((user) => (
+            <li
+              key={user.id}
+              className={`p-2 cursor-pointer ${
+                selectedUserId === user.id ? "bg-teal-600 text-white" : ""
+              } hover:bg-teal-100 transition-colors`}
+              onClick={() => handleUserClick(user)}
+            >
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </aside>
     </>
-   
   );
 };
 
